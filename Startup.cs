30fe90b2da4d48b17b98290dd4a33421bf2ef9b3
Tomasz Jagusz <tomasz.jagusz@yologroup.pl>
2018-10-18 13:06:19 +0200

    Update to .Net CORE 2.1

--- Startup.cs
+++ Startup.cs
@@ -7,7 +7,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OData;
+using Microsoft.OData.Edm;
 using Microsoft.OData.UriParser;
+using System;
 using System.Collections.Generic;
 
 namespace CoreTest1
@@ -24,6 +26,7 @@ namespace CoreTest1
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
+			//services.AddRouting(options => options.LowercaseUrls = true);
 			services.AddMvc();
 			services.AddOData();
 			services.AddODataQueryFilter();
@@ -37,25 +40,84 @@ namespace CoreTest1
 				app.UseDeveloperExceptionPage();
 			}
 
-			var modelBuilder = new ODataConventionModelBuilder(app.ApplicationServices);
-			modelBuilder.EntitySet<Product>("Products");
+            var modelBuilder = new ODataConventionModelBuilder(app.ApplicationServices);
+            modelBuilder.EntitySet<Product>("Products");
 
-			app.UseMvc(builder =>
-			{
-				builder.MapODataServiceRoute("ODataRoute", "odata", containerBuilder =>
-					containerBuilder.AddService(Microsoft.OData.ServiceLifetime.Singleton, sp => modelBuilder.GetEdmModel())
-						.AddService<IEnumerable<IODataRoutingConvention>>(Microsoft.OData.ServiceLifetime.Singleton, sp => ODataRoutingConventions.CreateDefaultWithAttributeRouting("ODataRoute", builder))
-						.AddService<ODataUriResolver>(Microsoft.OData.ServiceLifetime.Singleton, sp => new StringAsEnumResolver()));
-						//.AddService<ODataUriResolver>(Microsoft.OData.ServiceLifetime.Singleton, sp => new UnqualifiedODataUriResolver { EnableCaseInsensitive = true }));
+            app.UseMvc(builder =>
+            {
+                builder.MapODataServiceRoute("ODataRoute", "odata", containerBuilder =>
+                    containerBuilder.AddService(Microsoft.OData.ServiceLifetime.Singleton, sp => modelBuilder.GetEdmModel())
+                        .AddService<IEnumerable<IODataRoutingConvention>>(Microsoft.OData.ServiceLifetime.Singleton, sp => ODataRoutingConventions.CreateDefaultWithAttributeRouting("ODataRoute", builder))
+                        //.AddService<ODataUriResolver>(Microsoft.OData.ServiceLifetime.Singleton, sp => new StringAsEnumResolver()));
+                        .AddService<ODataUriResolver>(Microsoft.OData.ServiceLifetime.Singleton, sp => new UnqualifiedCallAndEnumPrefixFreeResolver { EnableCaseInsensitive = true }));
+                //.AddService<ODataUriResolver>(Microsoft.OData.ServiceLifetime.Singleton, sp => new UnqualifiedODataUriResolver { EnableCaseInsensitive = true }));
 
-			builder.Filter();
-				builder.MaxTop(10);
-				//routeBuilder.Select();
-				//routeBuilder.OrderBy();
-				//routeBuilder.Count();
-				//routeBuilder.Expand();
-				//routeBuilder.SetDefaultQuerySettings(new Microsoft.AspNet.OData.Query.DefaultQuerySettings { EnableCount = true, EnableFilter = true, MaxTop = null, EnableOrderBy = true, EnableExpand = true, EnableSelect = true });
-			});
-		}
+                builder.Filter();
+                builder.MaxTop(10);
+                //routeBuilder.Select();
+                //routeBuilder.OrderBy();
+                //routeBuilder.Count();
+                //routeBuilder.Expand();
+                //routeBuilder.SetDefaultQuerySettings(new Microsoft.AspNet.OData.Query.DefaultQuerySettings { EnableCount = true, EnableFilter = true, MaxTop = null, EnableOrderBy = true, EnableExpand = true, EnableSelect = true });
+            });
+        }
 	}
+
+    public class UnqualifiedCallAndEnumPrefixFreeResolver : ODataUriResolver
+    {
+        private readonly StringAsEnumResolver _stringAsEnum = new StringAsEnumResolver();
+        private readonly UnqualifiedODataUriResolver _unqualified = new UnqualifiedODataUriResolver();
+
+        private bool _enableCaseInsensitive;
+
+        /// <inheritdoc/>
+        public override bool EnableCaseInsensitive
+        {
+            get
+            {
+                return _enableCaseInsensitive;
+            }
+            set
+            {
+                _enableCaseInsensitive = value;
+                _stringAsEnum.EnableCaseInsensitive = this._enableCaseInsensitive;
+                _unqualified.EnableCaseInsensitive = this._enableCaseInsensitive;
+            }
+        }
+
+        /// <inheritdoc/>
+        public override IEnumerable<IEdmOperation> ResolveBoundOperations(IEdmModel model, string identifier,
+            IEdmType bindingType)
+        {
+            return _unqualified.ResolveBoundOperations(model, identifier, bindingType);
+        }
+
+        /// <inheritdoc/>
+        public override void PromoteBinaryOperandTypes(BinaryOperatorKind binaryOperatorKind,
+            ref SingleValueNode leftNode, ref SingleValueNode rightNode, out IEdmTypeReference typeReference)
+        {
+            _stringAsEnum.PromoteBinaryOperandTypes(binaryOperatorKind, ref leftNode, ref rightNode, out typeReference);
+        }
+
+        /// <inheritdoc/>
+        public override IEnumerable<KeyValuePair<string, object>> ResolveKeys(IEdmEntityType type,
+            IDictionary<string, string> namedValues, Func<IEdmTypeReference, string, object> convertFunc)
+        {
+            return _stringAsEnum.ResolveKeys(type, namedValues, convertFunc);
+        }
+
+        /// <inheritdoc/>
+        public override IEnumerable<KeyValuePair<string, object>> ResolveKeys(IEdmEntityType type,
+            IList<string> positionalValues, Func<IEdmTypeReference, string, object> convertFunc)
+        {
+            return _stringAsEnum.ResolveKeys(type, positionalValues, convertFunc);
+        }
+
+        /// <inheritdoc/>
+        public override IDictionary<IEdmOperationParameter, SingleValueNode> ResolveOperationParameters(
+            IEdmOperation operation, IDictionary<string, SingleValueNode> input)
+        {
+            return _stringAsEnum.ResolveOperationParameters(operation, input);
+        }
+    }
 }
